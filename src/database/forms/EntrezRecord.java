279d4f9c53c0f3916e31f25f6bfea5f515fad634
adamtreister <adamtreister@gmail.com>
2015-11-15 11:02:58 -0800

    fixed stringutil duplication

--- src/database/forms/EntrezRecord.java
+++ src/database/forms/EntrezRecord.java
@@ -4,9 +4,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import bookclub.StringUtil;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import util.StringUtil;
 import xml.XMLElement;
 
 //http://www.ncbi.nlm.nih.gov/books/NBK25501/
